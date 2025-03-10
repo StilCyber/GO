dc291c57916dfb80fa473c1b6a74a7376e4c0e36
Stil <qr.ost@ya.ru>
2025-03-10 10:09:02 +0300

    связный список начало

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
-package main 
+package main
 
 import "fmt"
 
-type Vertex struct {
-	X int
-	Y int
+type ListNode struct {
+	Val  int
+	Next *ListNode 
 }
 
+var head = ListNode{Val: 1, Next: &two}
+
+var two = ListNode{Val: 2, Next: &three}
+
+var three = ListNode{Val: 3, Next: &four}
+
+var four = ListNode{Val: 4, Next: &five}
+
+var five = ListNode{Val: 5, Next: nil}
+
+
 func main() {
-	fmt.Println(Vertex{1,2})
+	fmt.Println(head)
+	fmt.Println(two)
+	fmt.Println(three)
+	fmt.Println(four)
+	fmt.Println(five)
+}
+
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    curr := head
+
+    for curr != nil {
+        tmp := curr
+        curr = curr.Next
+        tmp.Next = prev
+        prev = tmp
+    }
+
+    return prev
 }
