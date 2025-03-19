6ab59a1c2d47233a9ab5ba4689498849f1bec49d
Stil <karnozavr666@ya.ru>
2025-03-19 13:45:04 +0600

    начало горутин

--- main.go
+++ main.go
@@ -2,40 +2,10 @@ package main
 
 import "fmt"
 
-type ListNode struct {
-	Val  int
-	Next *ListNode 
-}
-
-var head = ListNode{Val: 1, Next: &two}
-
-var two = ListNode{Val: 2, Next: &three}
-
-var three = ListNode{Val: 3, Next: &four}
-
-var four = ListNode{Val: 4, Next: &five}
-
-var five = ListNode{Val: 5, Next: nil}
-
-
 func main() {
-	fmt.Println(head)
-	fmt.Println(two)
-	fmt.Println(three)
-	fmt.Println(four)
-	fmt.Println(five)
+	a := 5000
+	for i := 0; i < a; i++ {
+		go fmt.Println(i)
+	}
 }
 
-func reverseList(head *ListNode) *ListNode {
-    var prev *ListNode
-    curr := head
-
-    for curr != nil {
-        tmp := curr
-        curr = curr.Next
-        tmp.Next = prev
-        prev = tmp
-    }
-
-    return prev
-}
